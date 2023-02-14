be5157cde81da18871c0c12dc6428fd4b65464cc
zkhssb <z164907681@gmail.com>
2023-02-14 08:50:05 +0800

    DTO最后一部分 (完成)

---
+++ QQGuildBotFunc/Dto/Roles/MemberAddRoleBody.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using QQGuildBotFunc.Dto.Channels;
+using System.Threading.Tasks;
+
+namespace QQGuildBotFunc.Dto.Roles
+{
+    /// <summary>
+    /// MemberAddRoleBody 增加子频道管理员身份组时附加内容
+    /// </summary>
+    public class MemberAddRoleBody
+    {
+        /// <summary>
+        /// 子频道对象
+        /// </summary>
+        [JsonPropertyName("channel")]
+        public required Channel Channel { get; set; }
+    }
+}
---
+++ QQGuildBotFunc/Dto/Schedules/Schedule.cs
@@ -0,0 +1,48 @@
+﻿using QQGuildBotFunc.Dto.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QQGuildBotFunc.Dto.Schedules
+{
+    /// <summary>
+    /// Schedule 日程对象
+    /// </summary>
+    public class Schedule
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("id")]
+        public string? ID { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("start_timestamp")]
+        public string? StartTimestamp { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("end_timestamp")]
+        public string? EndTimestamp { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("jump_channel_id")]
+        public string? JumpChannelID { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("remind_type")]
+        public string? RemindType { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("creator")]
+        public Member? Creator { get; set; }
+    }
+}
