23866a65ff08ad768075e55356f2ecd685ab3cf0
zkhssb <z164907681@gmail.com>
2023-02-16 20:38:41 +0800

    使用了Polly的OpenApi实现 (待完善)

---
+++ QQGuildBotFunc/Interfaces/IOpenApi.cs
@@ -0,0 +1,14 @@
+﻿namespace QQGuildBotFunc.Interfaces
+{
+    public interface IOpenApi
+    {
+        /// <summary>
+        /// 当前版本
+        /// </summary>
+        int Version();
+        /// <summary>
+        /// 获取 lastTraceID id
+        /// </summary>
+        string TraceID();
+    }
+}
