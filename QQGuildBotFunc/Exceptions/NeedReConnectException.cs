7099b67cc1904edda468a5ac9afe1608fb3e548a
zkhssb <z164907681@gmail.com>
2023-02-16 20:38:22 +0800

    WebSocket系列错误 (为后续开发准备)

---
+++ QQGuildBotFunc/Exceptions/NeedReConnectException.cs
@@ -0,0 +1,16 @@
+﻿using QQGuildBotFunc.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QQGuildBotFunc.Exceptions
+{
+    public class NeedReConnectException : WebSocketExceptionBase
+    {
+        public NeedReConnectException(Session session) : base(session, "need reconnect")
+        {
+        }
+    }
+}
---
+++ QQGuildBotFunc/Exceptions/UrlInvalidException.cs
@@ -0,0 +1,11 @@
+﻿using QQGuildBotFunc.WebSocket;
+
+namespace QQGuildBotFunc.Exceptions
+{
+    public class UrlInvalidException : WebSocketExceptionBase
+    {
+        public UrlInvalidException(Session session) : base(session, "ws ap url is invalid")
+        {
+        }
+    }
+}
