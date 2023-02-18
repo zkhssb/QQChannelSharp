c06f9ee7ada81ecf10dc13250126cf40f3790f07
zkhssb <z164907681@gmail.com>
2023-02-18 21:55:13 +0800

    WebSocket

--- QQGuildBotFunc/Enumerations/OPCode.cs
+++ QQGuildBotFunc/Enumerations/OPCode.cs
@@ -2,5 +2,14 @@
 {
     public enum OPCode
     {
+        WSDispatchEvent = 0,
+        WSHeartbeat = 1,
+        WSIdentity = 2,
+        WSResume = 6,
+        WSReconnect = 7,
+        WSInvalidSession = 9,
+        WSHello = 10,
+        WSHeartbeatAck = 11,
+        HTTPCallbackAck = 12,
     }
 }
---
+++ QQGuildBotFunc/Exceptions/WebSocketClosedException.cs
@@ -0,0 +1,13 @@
+﻿using QQGuildBotFunc.WebSocket;
+
+namespace QQGuildBotFunc.Exceptions
+{
+    public class WebSocketClosedException : WebSocketExceptionBase
+    {
+        public int CloseCode { get; private set; }
+        public WebSocketClosedException(Session session, int closeCode) : base(session, $"ws closed/code:{closeCode}")
+        {
+            CloseCode = closeCode;
+        }
+    }
+}
