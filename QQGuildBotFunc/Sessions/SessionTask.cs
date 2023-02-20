c149e42002a381e52bc2b491fd9278f835669e83
zkhssb <z164907681@gmail.com>
2023-02-20 22:58:45 +0800

    任务信息

---
+++ QQGuildBotFunc/Sessions/SessionTask.cs
@@ -0,0 +1,11 @@
+﻿using QQGuildBotFunc.WebSocket;
+
+namespace QQGuildBotFunc.Sessions
+{
+    public class SessionTask
+    {
+        public required Session Session { get; init; }
+        public required Task Task { get; init; }
+        public required CancellationTokenSource CancellationTokenSource { get; init; }
+    }
+}
