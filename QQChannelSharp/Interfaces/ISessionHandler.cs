7bf02649c6fe674eeea01da94c9d071f840a8c7b
zkhssb <z164907681@gmail.com>
2023-02-21 20:23:09 +0800

    会话接口定义

---
+++ QQChannelSharp/Interfaces/ISessionHandler.cs
@@ -0,0 +1,13 @@
+﻿using QQChannelSharp.WebSocket;
+
+namespace QQChannelSharp.Interfaces
+{
+    public delegate Task SessionAsyncCallback(Session session);
+    public interface ISessionHandler : IDisposable
+    {
+        event SessionAsyncCallback NeedConnect; // 需要连接
+        event SessionAsyncCallback NeedDisconnect; // 需要关闭
+        void Start();
+        Task StopAsync();
+    }
+}
---
+++ QQChannelSharp/Interfaces/ISessionManager.cs
@@ -0,0 +1,14 @@
+﻿using QQChannelSharp.Dto.WebSocket;
+using QQChannelSharp.Sessions;
+
+namespace QQChannelSharp.Interfaces
+{
+    /// <summary>
+    /// 会话管理器接口
+    /// </summary>
+    public interface ISessionManager : IDisposable
+    {
+        Task StartAsync();
+        Dictionary<Guid, SessionInfo> Sessions();
+    }
+}
