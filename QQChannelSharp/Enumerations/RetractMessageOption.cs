56e6fd6a7c98c219bc7f2124840fa17db6b06708
zkhssb <z164907681@gmail.com>
2023-02-28 16:25:00 +0800

    枚举 撤回消息可选项

---
+++ QQChannelSharp/Enumerations/RetractMessageOption.cs
@@ -0,0 +1,13 @@
+﻿namespace QQChannelSharp.Enumerations
+{
+    /// <summary>
+    /// 撤回消息可选参数
+    /// </summary>
+    public enum RetractMessageOption
+    {
+        /// <summary>
+        /// 隐藏小灰条
+        /// </summary>
+        Hidetip = 1,
+    }
+}
