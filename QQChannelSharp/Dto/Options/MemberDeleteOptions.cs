59e74102373fff8f95a400876e6a1ef36f2b7b10
zkhssb <z164907681@gmail.com>
2023-02-28 16:05:18 +0800

    DTO 更正成员删除可选项名称并移动位置

---
+++ QQChannelSharp/Dto/Options/MemberDeleteOptions.cs
@@ -0,0 +1,36 @@
+﻿using QQChannelSharp.Enumerations;
+using System.Text.Json.Serialization;
+
+namespace QQChannelSharp.Dto.Options
+{
+    public class MemberDeleteOptions
+    {
+        /// <summary>
+        /// 将当前成员同时添加到频道黑名单中
+        /// </summary>
+        [JsonPropertyName("add_blacklist")]
+        public bool AddBlackList { get; set; }
+        /// <summary>
+        /// 删除成员时同时撤回消息
+        /// </summary>
+        [JsonPropertyName("delete_history_msg_days")]
+        public DeleteHistoryMsgDay DeleteHistoryMsgDays { get; set; }
+
+        /// <summary>
+        /// 将当前成员同时添加到频道黑名单中
+        /// </summary>
+        public MemberDeleteOptions WithAddBlackList(bool addBlackList)
+        {
+            AddBlackList = addBlackList;
+            return this;
+        }
+        /// <summary>
+        /// 删除成员时同时撤回消息
+        /// </summary>
+        public MemberDeleteOptions WithDeleteHistoryMsg(DeleteHistoryMsgDay deleteHistoryMsgDays)
+        {
+            DeleteHistoryMsgDays = deleteHistoryMsgDays;
+            return this;
+        }
+    }
+}
