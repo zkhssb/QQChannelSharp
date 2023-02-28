2b3c0ab1bd2ba3d2b015a91f433ae251d47a92fc
zkhssb <z164907681@gmail.com>
2023-02-28 15:40:28 +0800

    DTO 增加 创建消息类

---
+++ QQChannelSharp/Dto/Messages/MessageToCreate.cs
@@ -0,0 +1,52 @@
+﻿using QQChannelSharp.Dto.Keyboard;
+using QQChannelSharp.Dto.Markdown;
+using QQChannelSharp.Dto.MessageArks;
+using System.Text.Json.Serialization;
+
+namespace QQChannelSharp.Dto.Messages
+{
+    /// <summary>
+    /// 消息创建
+    /// </summary>
+    public class MessageToCreate
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("content")]
+        public string? Content { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("embed")]
+        public Embed? Embed { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("ark")]
+        public Ark? Ark { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("image")]
+        public string? Image { get; set; }
+
+        /// <summary>
+        /// 要回复的消息id，为空是主动消息，公域机器人会异步审核，不为空是被动消息，公域机器人会校验语料
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("msg_id")]
+        public string? MsgId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("message_reference")]
+        public MessageReference? MessageReference { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("markdown")]
+        public MarkdownMessage? Markdown { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("keyboard")]
+        public MessageKeyboard? Keyboard { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("event_id")]
+        public string? EventId { get; set; }
+    }
+}
