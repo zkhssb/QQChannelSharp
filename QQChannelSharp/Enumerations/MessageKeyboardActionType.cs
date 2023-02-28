55026c1765e55f0c2c789a0d59ce1bdcf06bc4bb
zkhssb <z164907681@gmail.com>
2023-02-28 15:39:37 +0800

    Enum 增加  按钮(操作/权限)类型

---
+++ QQChannelSharp/Enumerations/MessageKeyboardActionType.cs
@@ -0,0 +1,21 @@
+﻿namespace QQChannelSharp.Enumerations
+{
+    /// <summary>
+    /// 按钮操作类型
+    /// </summary>
+    public enum MessageKeyboardActionType
+    {
+        /// <summary>
+        /// http 或 小程序 客户端识别 schema, data字段为链接
+        /// </summary>
+        Url = 0,
+        /// <summary>
+        /// 回调互动回调地址, data 传给互动回调地址
+        /// </summary>
+        Callback = 1,
+        /// <summary>
+        /// at机器人, 根据 at_bot_show_channel_list 决定在当前频道或用户选择频道,自动在输入框 @bot data
+        /// </summary>
+        AtBot = 2,
+    }
+}
---
+++ QQChannelSharp/Enumerations/MessageKeyboardPermissionType.cs
@@ -0,0 +1,22 @@
+﻿namespace QQChannelSharp.Enumerations
+{
+    public enum MessageKeyboardPermissionType
+    {
+        /// <summary>
+        /// 仅指定这条消息的人可操作
+        /// </summary>
+        SpecifyUserIDs = 0,
+        /// <summary>
+        /// 仅频道管理者可操作
+        /// </summary>
+        Manager = 1,
+        /// <summary>
+        /// 所有人可操作
+        /// </summary>
+        All = 2,
+        /// <summary>
+        /// 指定身份组可操作
+        /// </summary>
+        SpecifyRoleIDs = 3,
+    }
+}
