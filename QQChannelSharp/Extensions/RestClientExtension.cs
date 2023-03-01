006473be3c0959ac3b2670eccbfa96712c932e0b
zkhssb <z164907681@gmail.com>
2023-03-01 15:33:58 +0800

    RestClient Api客户端扩展方法

---
+++ QQChannelSharp/Extensions/RestClientExtension.cs
@@ -0,0 +1,60 @@
+﻿using QQChannelSharp.Dto;
+using QQChannelSharp.OpenApi;
+using RestSharp;
+using System.Text.Json;
+
+namespace QQChannelSharp.Extensions
+{
+    public static class RestClientExtension
+    {
+        public static async Task<HttpResult<TResult>> ExecAsync<TResult>(this RestClient client, RestRequest request)
+            where TResult : class
+        {
+            var result = await client.ExecuteAsync(request);
+
+            if (null == result)
+                return new(default, new() { ErrorCode = -1, Message = "a" })
+                {
+                    IsSuccess = false,
+                    StatusCode = System.Net.HttpStatusCode.BadRequest,
+                    TraceId = string.Empty
+                };
+
+            TResult? data;
+            OpenApiError? error;
+
+            try
+            {
+                if (result.IsSuccessStatusCode)
+                {
+                    if (typeof(TResult) == typeof(EmptyObject))
+                        data = null;
+                    else
+                        data = JsonSerializer.Deserialize<TResult>(result?.Content ?? string.Empty);
+                    error = default;
+                }
+                else
+                {
+                    data = default;
+                    error = JsonSerializer.Deserialize<OpenApiError>(result?.Content ?? string.Empty);
+                }
+            }
+            catch (JsonException ex)
+            {
+                data = default;
+                error = new()
+                {
+                    ErrorCode = -1,
+                    Message = ex.Message
+                };
+            }
+
+            return new(data, error)
+            {
+                IsSuccess = result!.IsSuccessStatusCode,
+                StatusCode = result!.StatusCode,
+                TraceId = result!.GetTraceId()
+            };
+        }
+    }
+}
---
+++ QQChannelSharp/Extensions/RestResponseExtension.cs
@@ -0,0 +1,12 @@
+﻿using RestSharp;
+
+namespace QQChannelSharp.Extensions
+{
+    public static class RestResponseExtension
+    {
+        public static string GetTraceId(this RestResponse response)
+            => response.Headers
+                ?.Where(h => h.Name?.ToLower() == "X-Tps-trace-ID".ToLower())
+                .FirstOrDefault()?.Value?.ToString() ?? string.Empty;
+    }
+}
