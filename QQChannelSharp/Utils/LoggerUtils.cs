7ddaf744e4b90361157189ff325e7c6fb29ce3d5
zkhssb <z164907681@gmail.com>
2023-03-01 15:02:26 +0800

    日志工具类

---
+++ QQChannelSharp/Utils/LoggerUtils.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace QQChannelSharp.Utils
+{
+    public class LoggerUtils
+    {
+        private static readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(build =>
+        {
+            build.AddConsole();
+        });
+
+        /// <summary>
+        /// 创建一个新的日志实例
+        /// </summary>
+        public static ILogger<T> CreateLogger<T>()
+        {
+            return _loggerFactory.CreateLogger<T>();
+        }
+    }
+}
