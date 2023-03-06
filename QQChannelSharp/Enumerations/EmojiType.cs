ae054fa63dd5e0afe864f7f94070b4ab01c0ef1f
zkhssb <z164907681@gmail.com>
2023-03-06 16:08:43 +0800

    EmojiType QQ官方表情类型Id表

---
+++ QQChannelSharp/Enumerations/EmojiType.cs
@@ -0,0 +1,528 @@
+﻿namespace QQChannelSharp.Enumerations
+{
+    /// <summary>
+    /// QQ官方表情表
+    /// </summary>
+    public enum EmojiType
+    {
+        /// <summary>
+        /// 得意
+        /// </summary>
+        Proud = 4,
+        /// <summary>
+        /// 流泪
+        /// </summary>
+        Tearful = 5,
+        /// <summary>
+        /// 睡
+        /// </summary>
+        Sleeping = 8,
+        /// <summary>
+        /// 大哭
+        /// </summary>
+        Sobbing = 9,
+        /// <summary>
+        /// 尴尬
+        /// </summary>
+        Awkward = 10,
+        /// <summary>
+        /// 调皮
+        /// </summary>
+        Naughty = 12,
+        /// <summary>
+        /// 微笑
+        /// </summary>
+        Smiling = 14,
+        /// <summary>
+        /// 酷
+        /// </summary>
+        Cool = 16,
+        /// <summary>
+        /// 可爱
+        /// </summary>
+        Cute = 21,
+        /// <summary>
+        /// 傲慢
+        /// </summary>
+        Arrogant = 23,
+        /// <summary>
+        /// 饥饿
+        /// </summary>
+        Hungry = 24,
+        /// <summary>
+        /// 困
+        /// </summary>
+        Sleepy = 25,
+        /// <summary>
+        /// 惊恐
+        /// </summary>
+        Scared = 26,
+        /// <summary>
+        /// 流汗
+        /// </summary>
+        Sweating = 27,
+        /// <summary>
+        /// 憨笑
+        /// </summary>
+        Grinning = 28,
+        /// <summary>
+        /// 悠闲
+        /// </summary>
+        Relaxed = 29,
+        /// <summary>
+        /// 奋斗
+        /// </summary>
+        Fighting = 30,
+        /// <summary>
+        /// 疑问
+        /// </summary>
+        Doubtful = 32,
+        /// <summary>
+        /// 嘘
+        /// </summary>
+        Hushing = 33,
+        /// <summary>
+        /// 晕
+        /// </summary>
+        Dizzy = 34,
+        /// <summary>
+        /// 敲打
+        /// </summary>
+        Knocking = 38,
+        /// <summary>
+        /// 再见
+        /// </summary>
+        Goodbye = 39,
+        /// <summary>
+        /// 发抖
+        /// </summary>
+        Shaking = 41,
+        /// <summary>
+        /// 爱情
+        /// </summary>
+        Love = 42,
+        /// <summary>
+        /// 跳跳
+        /// </summary>
+        Jumping = 43,
+        /// <summary>
+        /// 拥抱
+        /// </summary>
+        Hugging = 49,
+        /// <summary>
+        /// 蛋糕
+        /// </summary>
+        Cake = 53,
+        /// <summary>
+        /// 咖啡
+        /// </summary>
+        Coffee = 60,
+        /// <summary>
+        /// 玫瑰
+        /// </summary>
+        Rose = 63,
+        /// <summary>
+        /// 爱心
+        /// </summary>
+        Heart = 66,
+        /// <summary>
+        /// 太阳
+        /// </summary>
+        Sun = 74,
+        /// <summary>
+        /// 月亮
+        /// </summary>
+        Moon = 75,
+        /// <summary>
+        /// 赞
+        /// </summary>
+        ThumbUp = 76,
+        /// <summary>
+        /// 握手
+        /// </summary>
+        Handshake = 78,
+        /// <summary>
+        /// 胜利
+        /// </summary>
+        Victory = 79,
+        /// <summary>
+        /// 飞吻
+        /// </summary>
+        BlowingKiss = 85,
+        /// <summary>
+        /// 西瓜
+        /// </summary>
+        Watermelon = 89,
+        /// <summary>
+        /// 冷汗
+        /// </summary>
+        ColdSweat = 96,
+        /// <summary>
+        /// 擦汗
+        /// </summary>
+        WipeSweat = 97,
+        /// <summary>
+        /// 抠鼻
+        /// </summary>
+        PickingNose = 98,
+        /// <summary>
+        /// 鼓掌
+        /// </summary>
+        Clap = 99,
+        /// <summary>
+        /// 糗大了
+        /// </summary>
+        Embarrassed = 100,
+        /// <summary>
+        /// 坏笑
+        /// </summary>
+        Grin = 101,
+        /// <summary>
+        /// 左哼哼
+        /// </summary>
+        LeftHum = 102,
+        /// <summary>
+        /// 右哼哼
+        /// </summary>
+        RightHum = 103,
+        /// <summary>
+        /// 哈欠
+        /// </summary>
+        Yawn = 104,
+        /// <summary>
+        /// 委屈
+        /// </summary>
+        Sad = 106,
+        /// <summary>
+        /// 左亲亲
+        /// </summary>
+        LeftKiss = 109,
+        /// <summary>
+        /// 可怜
+        /// </summary>
+        Pity = 111,
+        /// <summary>
+        /// 示爱
+        /// </summary>
+        ShowLove = 116,
+        /// <summary>
+        /// 抱拳
+        /// </summary>
+        Fist = 118,
+        /// <summary>
+        /// 拳头
+        /// </summary>
+        Punch = 120,
+        /// <summary>
+        /// 爱你
+        /// </summary>
+        ILoveYou = 122,
+        /// <summary>
+        /// NO
+        /// </summary>
+        No = 123,
+        /// <summary>
+        /// OK
+        /// </summary>
+        Ok = 124,
+        /// <summary>
+        /// 转圈
+        /// </summary>
+        Turn = 125,
+        /// <summary>
+        /// 挥手
+        /// </summary>
+        Wave = 129,
+        /// <summary>
+        /// 喝彩
+        /// </summary>
+        Cheer = 144,
+        /// <summary>
+        /// 棒棒糖
+        /// </summary>
+        Lollipop = 147,
+        /// <summary>
+        /// 茶
+        /// </summary>
+        Tea = 171,
+        /// <summary>
+        /// 泪奔
+        /// </summary>
+        Tears = 173,
+
+        /// <summary>
+        /// 无奈
+        /// </summary>
+        Helpless = 174,
+
+        /// <summary>
+        /// 卖萌
+        /// </summary>
+        Moe = 175,
+
+        /// <summary>
+        /// 小纠结
+        /// </summary>
+        Puzzled = 176,
+
+        /// <summary>
+        /// doge
+        /// </summary>
+        Doge = 179,
+
+        /// <summary>
+        /// 惊喜
+        /// </summary>
+        Surprised = 180,
+
+        /// <summary>
+        /// 骚扰
+        /// </summary>
+        Harassment = 181,
+
+        /// <summary>
+        /// 笑哭
+        /// </summary>
+        LaughAndCry = 182,
+
+        /// <summary>
+        /// 我最美
+        /// </summary>
+        Beautiful = 183,
+
+        /// <summary>
+        /// 点赞
+        /// </summary>
+        Like = 201,
+
+        /// <summary>
+        /// 托脸
+        /// </summary>
+        FaceTouch = 203,
+
+        /// <summary>
+        /// 托腮
+        /// </summary>
+        ChinTouch = 212,
+
+        /// <summary>
+        /// 啵啵
+        /// </summary>
+        Kissing = 214,
+
+        /// <summary>
+        /// 蹭一蹭
+        /// </summary>
+        Rub = 219,
+
+        /// <summary>
+        /// 抱抱
+        /// </summary>
+        Hug = 222,
+
+        /// <summary>
+        /// 拍手
+        /// </summary>
+        Hands = 227,
+
+        /// <summary>
+        /// 佛系
+        /// </summary>
+        Zen = 232,
+
+        /// <summary>
+        /// 喷脸
+        /// </summary>
+        SpitFace = 240,
+
+        /// <summary>
+        /// 甩头
+        /// </summary>
+        ShakeHead = 243,
+
+        /// <summary>
+        /// 加油抱抱
+        /// </summary>
+        FightingHug = 246,
+
+        /// <summary>
+        /// 脑阔疼
+        /// </summary>
+        BrainPain = 262,
+
+        /// <summary>
+        /// 捂脸
+        /// </summary>
+        FacePalm = 264,
+
+        /// <summary>
+        /// 辣眼睛
+        /// </summary>
+        EyeBurn = 265,
+
+        /// <summary>
+        /// 哦哟
+        /// </summary>
+        OhYeah = 266,
+
+        /// <summary>
+        /// 头秃
+        /// </summary>
+        Bald = 267,
+
+        /// <summary>
+        /// 问号脸
+        /// </summary>
+        Question = 268,
+
+        /// <summary>
+        /// 暗中观察
+        /// </summary>
+        Observe = 269,
+
+        /// <summary>
+        /// emm
+        /// </summary>
+        Hmm = 270,
+
+        /// <summary>
+        /// 吃瓜
+        /// </summary>
+        EatingMelon = 271,
+
+        /// <summary>
+        /// 呵呵哒
+        /// </summary>
+        Hehe = 272,
+
+        /// <summary>
+        /// 我酸了
+        /// </summary>
+        Sour = 273,
+
+        /// <summary>
+        /// 汪汪
+        /// </summary>
+        WoofWoof = 277,
+
+        /// <summary>
+        /// 汗
+        /// </summary>
+        Sweat = 278,
+
+        /// <summary>
+        /// 无眼笑
+        /// </summary>
+        EyelessSmile = 281,
+
+        /// <summary>
+        /// 敬礼
+        /// </summary>
+        Salute = 282,
+
+        /// <summary>
+        /// 面无表情
+        /// </summary>
+        Expressionless = 284,
+
+        /// <summary>
+        /// 摸鱼
+        /// </summary>
+        SlackOff = 285,
+
+        /// <summary>
+        /// 哦
+        /// </summary>
+        Oh = 287,
+
+        /// <summary>
+        /// 睁眼
+        /// </summary>
+        OpenEyes = 289,
+
+        /// <summary>
+        /// 敲开心
+        /// </summary>
+        KnockingHeart = 290,
+
+        /// <summary>
+        /// 摸锦鲤
+        /// </summary>
+        TouchingKoi = 293,
+
+        /// <summary>
+        /// 期待
+        /// </summary>
+        Anticipation = 294,
+
+        /// <summary>
+        /// 拜谢
+        /// </summary>
+        ThankYou = 297,
+
+        /// <summary>
+        /// 元宝
+        /// </summary>
+        GoldIngot = 298,
+
+        /// <summary>
+        /// 牛啊
+        /// </summary>
+        Awesome = 299,
+
+        /// <summary>
+        /// 右亲亲
+        /// </summary>
+        RightKiss = 305,
+
+        /// <summary>
+        /// 牛气冲天
+        /// </summary>
+        Mighty = 306,
+
+        /// <summary>
+        /// 喵喵
+        /// </summary>
+        Meow = 307,
+
+        /// <summary>
+        /// 仔细分析
+        /// </summary>
+        Analyzing = 314,
+
+        /// <summary>
+        /// 加油
+        /// </summary>
+        Encourage = 315,
+
+        /// <summary>
+        /// 崇拜
+        /// </summary>
+        Admire = 318,
+
+        /// <summary>
+        /// 比心
+        /// </summary>
+        FingerHeart = 319,
+
+        /// <summary>
+        /// 庆祝
+        /// </summary>
+        Celebration = 320,
+
+        /// <summary>
+        /// 拒绝
+        /// </summary>
+        Refuse = 322,
+
+        /// <summary>
+        /// 吃糖
+        /// </summary>
+        EatingCandy = 324,
+
+        /// <summary>
+        /// 生气
+        /// </summary>
+        Angry = 326
+    }
+}
