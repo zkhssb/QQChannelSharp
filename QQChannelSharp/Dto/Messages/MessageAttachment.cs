f56238765255ecf77dcb9e0aa08686619ad6993c
zkhssb <z164907681@gmail.com>
2023-03-08 13:56:38 +0800

    消息附件 更新DTO

--- QQChannelSharp/Dto/Messages/MessageAttachment.cs
+++ QQChannelSharp/Dto/Messages/MessageAttachment.cs
@@ -7,7 +7,35 @@ namespace QQChannelSharp.Dto.Messages
     /// </summary>
     public class MessageAttachment
     {
+        /// <summary>
+        /// 附件类型, 如:
+        /// image/gif, image/png, image/jpg
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("content_type")]
+        public string? ContentType { get; set; }
+
         [JsonPropertyName("url")]
         public required string URL { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("filename")]
+        public string? FileName { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("height")]
+        public int? Height { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("width")]
+        public int? Width { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("size")]
+        public int? Size { get; set; }
     }
 }
