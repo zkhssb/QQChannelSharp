2996b0d1feb54629407ba0f501f188a02c96c8d2
zkhssb <z164907681@gmail.com>
2023-03-19 21:39:01 +0800

    Update Session增加Json特性

--- QQChannelSharp/WebSocket/Session.cs
+++ QQChannelSharp/WebSocket/Session.cs
@@ -1,16 +1,25 @@
-﻿namespace QQChannelSharp.WebSocket
+﻿using System.Text.Json.Serialization;
+
+namespace QQChannelSharp.WebSocket
 {
     /// <summary>
     /// Session 连接的 session 结构，包括链接的所有必要字段
     /// </summary>
     public class Session
     {
+        [JsonPropertyName("guid")]
         public required Guid Guid { get; init; }
+        [JsonPropertyName("shard")]
         public required ShardConfig Shard { get; set; }
+        [JsonPropertyName("id")]
         public required string Id { get; set; }
+        [JsonPropertyName("url")]
         public required string Url { get; set; }
+        [JsonPropertyName("bot_info")]
         public required ChannelBotInfo BotInfo { get; set; }
+        [JsonPropertyName("intent")]
         public required int Intent { get; set; }
+        [JsonPropertyName("seq")]
         public required long LastSeq { get; set; }
     }
 }
--- QQChannelSharp/WebSocket/ShardConfig.cs
+++ QQChannelSharp/WebSocket/ShardConfig.cs
@@ -1,11 +1,15 @@
-﻿namespace QQChannelSharp.WebSocket
+﻿using System.Text.Json.Serialization;
+
+namespace QQChannelSharp.WebSocket
 {
     /// <summary>
     /// ShardConfig 连接的 shard 配置，ShardID 从 0 开始，ShardCount 最小为 1
     /// </summary>
     public class ShardConfig
     {
+        [JsonPropertyName("shard_id")]
         public required int ShardID { get; set; }
+        [JsonPropertyName("shard_count")]
         public int ShardCount { get; set; } = 1;
     }
 }
