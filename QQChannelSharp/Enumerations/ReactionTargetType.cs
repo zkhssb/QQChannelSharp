d10441160e98b1bd377c58d77a1c085119432547
Nukoooo <me@nuko.me>
2023-10-14 22:56:27 +0800

    fix

--- QQChannelSharp/Enumerations/ReactionTargetType.cs
+++ QQChannelSharp/Enumerations/ReactionTargetType.cs
@@ -1,4 +1,6 @@
-﻿namespace QQChannelSharp.Enumerations
+﻿using System.Runtime.Serialization;
+
+namespace QQChannelSharp.Enumerations
 {
     /// <summary>
     /// 表情表态对象类型
@@ -8,18 +10,22 @@
         /// <summary>
         /// 消息
         /// </summary>
+        [EnumMember(Value = "ReactionTargetType_MSG")]
         Msg = 0,
         /// <summary>
         /// 帖子
         /// </summary>
+        [EnumMember(Value = "ReactionTargetType_FEED")]
         Feed = 1,
         /// <summary>
         /// 评论
         /// </summary>
+        [EnumMember(Value = "ReactionTargetType_COMMNENT")] // 有错字, 应该是 ReactionTargetType_COMMENT
         Comment = 2,
         /// <summary>
         /// 回复
         /// </summary>
+        [EnumMember(Value = "ReactionTargetType_REPLY")]
         Reply = 3,
     }
 }
