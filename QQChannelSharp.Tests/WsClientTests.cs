a4e31c03699f0eb01ff0368b4299ee4852eebc62
zkhssb <z164907681@gmail.com>
2023-11-17 08:19:04 +0800

    add: WsClient单元测试,异常处理

---
+++ QQChannelSharp.Tests/WsClientTests.cs
@@ -0,0 +1,24 @@
+﻿using QQChannelSharp.Client;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+
+namespace QQChannelSharp.Tests;
+[TestClass]
+public class WsClientTests
+{
+    [TestMethod]
+    public async Task WebSocketClientErrorTest()
+    {
+        WsClient client = new(new()
+        {
+            BotInfo = new("", "", true),
+            Guid = Guid.NewGuid(),
+            Id = "",
+            Intent = 1,
+            LastSeq = 0,
+            Shard = new() { ShardID = 0 },
+            Url = "wss://这就是个不存在的地址.com/"
+        });
+        await Assert.ThrowsExceptionAsync<WebSocketException>(client.ConnectAsync);
+    }
+}
